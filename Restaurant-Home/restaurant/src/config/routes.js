a7a42242c4fab881699fb0ce285bcd57b8c043b0
Phuong <phuongphuong262204@gmail.com>
2026-07-18 13:38:28 +0700

    Initial update
    update for lost process

---
+++ Restaurant-Home/restaurant/src/config/routes.js
@@ -0,0 +1,46 @@
+const routes = {
+    // Home routes
+    home: "/",
+    // AboutUs and ContactUs routes
+    aboutus: "/about",
+    contactus: "/contact",
+    //404
+    error: "/error",
+    //Menu
+    menu: "/menu",
+    //Chef
+    chef: "/chef",
+    chefDetail: "/chef-detail",
+
+    //Blog
+    blog: "/blog",
+    blogDetail: "/blog-detail",
+
+    //Shop Food
+    shop: "/shop",
+    foodShopDetail: "/shop-detail/:id",
+    wishlistPage: "/wishlist",
+
+    // Cart
+    cartTab: "/cart",
+    checkOut:"/check_out",
+    order:"/order",
+    orderConfirm: "/order_confirm/:id",
+    orderList: "/orderList",
+
+
+    // Reservation
+    bookTable: "/book_table",
+    bookingConfirm: "/booking_confirm/:id",
+    bookingList: "/booking_list",
+
+    // orderFood: "/order_food",
+
+    // Authentication routes
+    profile: "/profile",
+    login: "/login",
+    forgot_password: "/forgot-password",
+    reset_password: "/reset-password/:resetToken",
+
+};
+export default routes;
